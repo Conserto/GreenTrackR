ea8588d3c6b2a8ff44ac21e97a717d16bc98a67e
Maxime R <maxime.riviere@conserto.pro>
2024-03-01 15:15:14 +0100

    remove comment

--- src/const/measure.const.ts
+++ src/const/measure.const.ts
@@ -3,4 +3,4 @@ export const CAR_AVERAGE_CO2 = 220;
 export const PHONE_AVERAGE_CO2 = 8.3;
 export const KWH_PER_REQUEST_DATA_CENTER = 4.66e-4;
 export const KWH_PER_BYTE_NETWORK = 1.46e-8;
-export const KWH_DEVICE = 0.0075; //pendant 1e minute
+export const KWH_DEVICE = 0.0075;
