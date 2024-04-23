6c6793e56e3a635374854cb7932c38b6f567a763
Vincent Drouard <vincent.drouard@conserto.pro>
2024-04-23 17:27:39 +0200

    Add kpi

--- src/components/Synthesis.svelte
+++ src/components/Synthesis.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import type { Measure } from '../interface';
   import Tooltip from './Tooltip.svelte';
-  import { formatNbRequest, formatNumber, formatSizeTransferred } from '../utils';
+  import { formatNbRequest, formatNumber, formatSizeTransferred, translate } from '../utils';
   import { SynthesisSrv } from '../service/synthesis.service';
+  import { AdpIcon, EauIcon, ElecIcon } from '../assets/icons';
 
   export let datas: Measure[];
 
@@ -64,8 +65,11 @@
           <Tooltip translateKey='synTabHeadScore' />
         </th>
         <th class="{head.classe}" scope="col" colspan="1">
-          <Tooltip translateKey='synTabHeadGrade' />
+          <Tooltip translateKey='synTabHeadKpi' />
         </th>
+        <!--<th class="{head.classe}" scope="col" colspan="1">
+          <Tooltip translateKey='synTabHeadGrade' />
+        </th>-->
       {/each}
     </tr>
     </thead>
@@ -82,39 +86,87 @@
         <td class="scroll">{formatNumber(page.sScroll.ges.pageTotal)}</td>
         {#if page.sScroll.score?.value}
           <td class="scroll"
-              style="background-color: {page.sScroll.score?.color}; color: {page.sScroll.score?.textColor}">{formatNumber(page.sScroll.score.value)}</td>
-          <td class="scroll"
-              style="background-color: {page.sScroll.score?.color}; color: {page.sScroll.score?.textColor}">{page.sScroll.score.gradeLetter}</td>
+              style="background-color: {page.sScroll.score?.color}; color: {page.sScroll.score?.textColor}">{formatNumber(page.sScroll.score.value)}
+            ({page.sScroll.score.gradeLetter})
+          </td>
+          <!--<td class="scroll"
+              style="background-color: {page.sScroll.score?.color}; color: {page.sScroll.score?.textColor}">{page.sScroll.score.gradeLetter}</td>-->
         {:else }
           <td class="scroll"></td>
-          <td class="scroll"></td>
+          <!--<td class="scroll"></td>-->
         {/if}
+        <td class="scroll">
+          <div class="kpi"><img src="{EauIcon}" class="icoWu" alt="water_icon" />
+            <Tooltip top="{true}" value="{formatNumber(page.sScroll.wu.pageTotal)}"
+                     tooltipValue={translate("wuEquivalent")} />
+          </div>
+          <div class="kpi"><img src="{AdpIcon}" class="icoAdp" alt="adp_icon" />
+            <Tooltip top="{true}" value="{formatNumber(page.sScroll.adpe.pageTotal)}"
+                     tooltipValue={translate("adpeEquivalent")} />
+          </div>
+          <div class="kpi"><img src="{ElecIcon}" class="icoElec" alt="elec_icon" />
+            <Tooltip top="{true}" value="{formatNumber(page.sScroll.energy.kWhPage)}"
+                     tooltipValue={translate("energyEquivalent")} />
+          </div>
+        </td>
         <td>{page.sClick.count}</td>
         <td>{formatSizeTransferred(page.sClick.network.network.size, page.sClick.network.network.sizeUncompress)}</td>
         <td>{formatNbRequest(page.sClick.network.nbRequest, page.sClick.network.nbRequestCache)}</td>
         <td>{formatNumber(page.sClick.ges.pageTotal)}</td>
         {#if page.sClick.score?.value}
           <td
-            style="background-color: {page.sClick.score?.color}; color: {page.sClick.score?.textColor}">{formatNumber(page.sClick.score.value)}</td>
-          <td
-            style="background-color: {page.sClick.score?.color}; color: {page.sClick.score?.textColor}">{page.sClick.score.gradeLetter}</td>
+            style="background-color: {page.sClick.score?.color}; color: {page.sClick.score?.textColor}">{formatNumber(page.sClick.score.value)}
+            ({page.sClick.score.gradeLetter})
+          </td>
+          <!--<td
+            style="background-color: {page.sClick.score?.color}; color: {page.sClick.score?.textColor}">{page.sClick.score.gradeLetter}</td>-->
         {:else }
           <td></td>
-          <td></td>
+          <!--<td></td>-->
         {/if}
+        <td>
+          <div class="kpi"><img src="{EauIcon}" class="icoWu" alt="water_icon" />
+            <Tooltip top="{true}" value="{formatNumber(page.sClick.wu.pageTotal)}"
+                     tooltipValue={translate("wuEquivalent")} />
+          </div>
+          <div class="kpi"><img src="{AdpIcon}" class="icoAdp" alt="adp_icon" />
+            <Tooltip top="{true}" value="{formatNumber(page.sClick.adpe.pageTotal)}"
+                     tooltipValue={translate("adpeEquivalent")} />
+          </div>
+          <div class="kpi"><img src="{ElecIcon}" class="icoElec" alt="elec_icon" />
+            <Tooltip top="{true}" value="{formatNumber(page.sClick.energy.kWhPage)}"
+                     tooltipValue={translate("energyEquivalent")} />
+          </div>
+        </td>
         <td
           class="page">{formatSizeTransferred(page.sPage.network.network.size, page.sPage.network.network.sizeUncompress)}</td>
         <td class="page">{formatNbRequest(page.sPage.network.nbRequest, page.sPage.network.nbRequestCache)}</td>
         <td class="page">{formatNumber(page.sPage.ges.pageTotal)}</td>
         {#if page.sPage.score?.value}
           <td class="page"
-              style="background-color: {page.sPage.score?.color}; color: {page.sPage.score?.textColor}">{formatNumber(page.sPage.score.value)}</td>
-          <td class="page"
-              style="background-color: {page.sPage.score?.color}; color: {page.sPage.score?.textColor}">{page.sPage.score.gradeLetter}</td>
+              style="background-color: {page.sPage.score?.color}; color: {page.sPage.score?.textColor}">{formatNumber(page.sPage.score.value)}
+            ({page.sPage.score.gradeLetter})
+          </td>
+          <!--<td class="page"
+              style="background-color: {page.sPage.score?.color}; color: {page.sPage.score?.textColor}">{page.sPage.score.gradeLetter}</td>-->
         {:else }
           <td class="page"></td>
-          <td class="page"></td>
+          <!--<td class="page"></td>-->
         {/if}
+        <td class="page">
+          <div class="kpi"><img src="{EauIcon}" class="icoWu" alt="water_icon" />
+            <Tooltip top="{true}" value="{formatNumber(page.sPage.wu.pageTotal)}"
+                     tooltipValue={translate("wuEquivalent")} />
+          </div>
+          <div class="kpi"><img src="{AdpIcon}" class="icoAdp" alt="adp_icon" />
+            <Tooltip top="{true}" value="{formatNumber(page.sPage.adpe.pageTotal)}"
+                     tooltipValue={translate("adpeEquivalent")} />
+          </div>
+          <div class="kpi"><img src="{ElecIcon}" class="icoElec" alt="elec_icon" />
+            <Tooltip top="{true}" value="{formatNumber(page.sPage.energy.kWhPage)}"
+                     tooltipValue={translate("energyEquivalent")} />
+          </div>
+        </td>
       </tr>
     {/each}
     </tbody>
@@ -131,26 +183,58 @@
       <th class="scroll">{formatNumber(synthesis.total.sScroll.ges.pageTotal)}</th>
       {#if synthesis.total.sScroll.score?.value}
         <th class="scroll"
-            style="background-color: {synthesis.total.sScroll.score?.color}; color: {synthesis.total.sScroll.score?.textColor}">{formatNumber(synthesis.total.sScroll.score.value)}</th>
-        <th class="scroll"
-            style="background-color: {synthesis.total.sScroll.score?.color}; color: {synthesis.total.sScroll.score?.textColor}">{synthesis.total.sScroll.score.gradeLetter}</th>
+            style="background-color: {synthesis.total.sScroll.score?.color}; color: {synthesis.total.sScroll.score?.textColor}">{formatNumber(synthesis.total.sScroll.score.value)}
+          ({synthesis.total.sScroll.score.gradeLetter})
+        </th>
+        <!--<th class="scroll"
+            style="background-color: {synthesis.total.sScroll.score?.color}; color: {synthesis.total.sScroll.score?.textColor}">{synthesis.total.sScroll.score.gradeLetter}</th>-->
       {:else }
         <th class="scroll"></th>
-        <th class="scroll"></th>
+        <!--<th class="scroll"></th>-->
       {/if}
+      <th class="scroll">
+        <div class="kpi"><img src="{EauIcon}" class="icoWu" alt="water_icon" />
+          <Tooltip top="{true}" value="{formatNumber(synthesis.total.sScroll.wu.pageTotal)}"
+                   tooltipValue={translate("wuEquivalent")} />
+        </div>
+        <div class="kpi"><img src="{AdpIcon}" class="icoAdp" alt="adp_icon" />
+          <Tooltip top="{true}" value="{formatNumber(synthesis.total.sScroll.adpe.pageTotal)}"
+                   tooltipValue={translate("adpeEquivalent")} />
+        </div>
+        <div class="kpi"><img src="{ElecIcon}" class="icoElec" alt="elec_icon" />
+          <Tooltip top="{true}" value="{formatNumber(synthesis.total.sScroll.energy.kWhPage)}"
+                   tooltipValue={translate("energyEquivalent")} />
+        </div>
+      </th>
       <th>{synthesis.total.sClick.count}</th>
       <th>{formatSizeTransferred(synthesis.total.sClick.network.network.size, synthesis.total.sClick.network.network.sizeUncompress)}</th>
       <th>{formatNbRequest(synthesis.total.sClick.network.nbRequest, synthesis.total.sClick.network.nbRequestCache)}</th>
       <th>{formatNumber(synthesis.total.sClick.ges.pageTotal)}</th>
       {#if synthesis.total.sClick.score?.value}
         <th
-          style="background-color: {synthesis.total.sClick.score?.color}; color: {synthesis.total.sClick.score?.textColor}">{formatNumber(synthesis.total.sClick.score.value)}</th>
-        <th
-          style="background-color: {synthesis.total.sClick.score?.color}; color: {synthesis.total.sClick.score?.textColor}">{synthesis.total.sClick.score.gradeLetter}</th>
+          style="background-color: {synthesis.total.sClick.score?.color}; color: {synthesis.total.sClick.score?.textColor}">{formatNumber(synthesis.total.sClick.score.value)}
+          ({synthesis.total.sClick.score.gradeLetter})
+        </th>
+        <!--<th
+          style="background-color: {synthesis.total.sClick.score?.color}; color: {synthesis.total.sClick.score?.textColor}">{synthesis.total.sClick.score.gradeLetter}</th>-->
       {:else }
         <th></th>
-        <th></th>
+        <!--<th></th>-->
       {/if}
+      <th>
+        <div class="kpi"><img src="{EauIcon}" class="icoWu" alt="water_icon" />
+          <Tooltip top="{true}" value="{formatNumber(synthesis.total.sClick.wu.pageTotal)}"
+                   tooltipValue={translate("wuEquivalent")} />
+        </div>
+        <div class="kpi"><img src="{AdpIcon}" class="icoAdp" alt="adp_icon" />
+          <Tooltip top="{true}" value="{formatNumber(synthesis.total.sClick.adpe.pageTotal)}"
+                   tooltipValue={translate("adpeEquivalent")} />
+        </div>
+        <div class="kpi"><img src="{ElecIcon}" class="icoElec" alt="elec_icon" />
+          <Tooltip top="{true}" value="{formatNumber(synthesis.total.sClick.energy.kWhPage)}"
+                   tooltipValue={translate("energyEquivalent")} />
+        </div>
+      </th>
       <th
         class="page">{formatSizeTransferred(synthesis.total.sPage.network.network.size, synthesis.total.sPage.network.network.sizeUncompress)}</th>
       <th
@@ -158,13 +242,29 @@
       <th class="page">{formatNumber(synthesis.total.sPage.ges.pageTotal)}</th>
       {#if synthesis.total.sPage.score?.value}
         <th class="page"
-            style="background-color: {synthesis.total.sPage.score?.color}; color: {synthesis.total.sPage.score?.textColor}">{formatNumber(synthesis.total.sPage.score.value)}</th>
-        <th class="page"
-            style="background-color: {synthesis.total.sPage.score?.color}; color: {synthesis.total.sPage.score?.textColor}">{synthesis.total.sPage.score.gradeLetter}</th>
+            style="background-color: {synthesis.total.sPage.score?.color}; color: {synthesis.total.sPage.score?.textColor}">{formatNumber(synthesis.total.sPage.score.value)}
+          ({synthesis.total.sPage.score.gradeLetter})
+        </th>
+        <!--<th class="page"
+            style="background-color: {synthesis.total.sPage.score?.color}; color: {synthesis.total.sPage.score?.textColor}">{synthesis.total.sPage.score.gradeLetter}</th>-->
       {:else }
         <th class="page"></th>
-        <th class="page"></th>
+        <!--<th class="page"></th>-->
       {/if}
+      <th class="scroll">
+        <div class="kpi"><img src="{EauIcon}" class="icoWu" alt="water_icon" />
+          <Tooltip top="{true}" value="{formatNumber(synthesis.total.sPage.wu.pageTotal)}"
+                   tooltipValue={translate("wuEquivalent")} />
+        </div>
+        <div class="kpi"><img src="{AdpIcon}" class="icoAdp" alt="adp_icon" />
+          <Tooltip top="{true}" value="{formatNumber(synthesis.total.sPage.adpe.pageTotal)}"
+                   tooltipValue={translate("adpeEquivalent")} />
+        </div>
+        <div class="kpi"><img src="{ElecIcon}" class="icoElec" alt="elec_icon" />
+          <Tooltip top="{true}" value="{formatNumber(synthesis.total.sPage.energy.kWhPage)}"
+                   tooltipValue={translate("energyEquivalent")} />
+        </div>
+      </th>
     </tr>
     </tfoot>
   </table>
@@ -177,6 +277,17 @@
     box-shadow: var(--box-shadow--sm), var(--box-shadow--sm);
     border-radius: 8px;
     padding-bottom: 0.5em;
+
+    div.kpi {
+      white-space: nowrap;
+
+      img {
+        &.icoWu, &.icoAdp, &.icoElec {
+          width: 1em;
+          margin-right: 0.2em;
+        }
+      }
+    }
 
     .table {
       border-collapse: collapse;
