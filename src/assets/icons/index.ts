aeffe5faa7dbcec93e10fb9665a0b2510296b033
Vincent Drouard <vincent.drouard@conserto.pro>
2024-04-24 13:12:49 +0200

    Update img

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,6 +2,6 @@ export { default as CheckIcon } from './CheckIcon.svelte';
 export { default as WarnIcon } from './WarnIcon.svelte';
 export { default as PhoneIcon } from './phone.webp';
 export { default as CarIcon } from './car.webp';
-export { default as EauIcon } from './goute.png';
+export { default as EauIcon } from './goute.webp';
 export { default as ElecIcon } from './elec.webp';
-export { default as AdpIcon } from './adp.svg';
+export { default as AdpIcon } from './adp.webp';
