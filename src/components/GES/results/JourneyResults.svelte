d37f9078dec43d2650b666f8028e7ddafe063299
OlivierCamon <olivier.camon@conserto.pro>
2024-05-16 17:06:41 +0200

    fix #48 : properly handle complex table.

--- src/components/GES/results/JourneyResults.svelte
+++ src/components/GES/results/JourneyResults.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+  <script lang="ts">
   import type { Measure } from 'src/interface';
   import { formatGesMeasuresForTable, translate } from 'src/utils/utils';
   import { SynthesisTable, Table } from 'src/components';
@@ -9,15 +9,7 @@
   const dataFormatted = formatGesMeasuresForTable(measures);
 </script>
 
-<h4 class="table-caption">{translate('messageResults')}</h4>
-<SynthesisTable datas={measures} />
+<SynthesisTable datas={measures} caption={translate('messageResults')} description={translate('descriptionResults')}/>
 <br/>
-<br/>
-<h4 class="table-caption">{translate('messageResultsDetail')}</h4>
-<Table columnHeaders={gesTableHeaders} datas={dataFormatted} />
 
-<style lang="scss">
-  .table-caption {
-    text-align: center;
-  }
-</style>
+<Table caption={translate('messageResultsDetail')} description={translate('descriptionResultsDetail')} columnHeaders={gesTableHeaders} datas={dataFormatted} />
